955020bfca8b04200d0d3d5909da86e8146894bf
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-07-05 19:10:27 +0100

    fix syntax

--- 0x06-regular_expressions/0-simply_match_holberton.rb
+++ 0x06-regular_expressions/0-simply_match_holberton.rb
@@ -3,8 +3,8 @@ if ARGV.length != 1
 	puts "we need exactly one argument"
 	exit
 end
-pattren = ARGV[0].scan(/Holbrton/)
-for i in pattren do
+pattren = ARGV[0].scan(/Holberton/)
+for i in pattren
 	print i
 end
 print"\n"
