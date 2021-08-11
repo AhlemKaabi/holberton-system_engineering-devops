d7ccee76721ff3848193c42fd113f1f783bfb991
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-08-11 21:57:05 +0100

    fix path

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # create a manifest that kills a process named killmenow.
 exec { 'killmenow':
     command => 'pkill -f killmenow',
-    path    => '/usr/bin/pkill',
+    path    => '/usr/bin:/usr/sbin:/bin'
   }
