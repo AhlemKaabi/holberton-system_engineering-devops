e0335a1acb8765de1476b88d48e76bef65083118
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-09-15 18:44:51 +0100

    fix pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,14 +12,13 @@ if __name__ == "__main__":
     number_completed_todos = 0
     number_of_todos = 0
     completed_tasks = []
-
-    user_url = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
+    users_url = 'https://jsonplaceholder.typicode.com/users/'
+    user_url = users_url + sys.argv[1]
     user_infos = requests.get(user_url).json()
 
     user_name = user_infos.get("name")
 
-    user_todos_url = 'https://jsonplaceholder.typicode.com/users/'
-    + sys.argv[1] + '/todos/'
+    user_todos_url = users_url + sys.argv[1] + '/todos/'
     user_todos = requests.get(user_todos_url).json()
 
     for user_todo in user_todos:
@@ -28,7 +27,7 @@ if __name__ == "__main__":
             number_completed_todos += 1
             completed_tasks.append(user_todo.get("title"))
 
-    print("Employee {} is done with taks({}/{}):".format(
+    print("Employee {} is done with tasks({}/{}):".format(
         user_name,
         number_completed_todos,
         number_of_todos
